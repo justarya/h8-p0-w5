a8886cffcdeaa96dbca4aa5391ddfd593af09ccb
A R Y A <just1arya@gmail.com>
2019-06-02 19:59:59 +0700

    Add exercise 7,8

---
+++ exercise-8.js
@@ -0,0 +1,13 @@
+function cariPelaku(str) {
+  // you can only write your code here!
+  hasil = str.match(/(abc)/g) || [];
+
+  return hasil.length;
+}
+
+// TEST CASES
+console.log(cariPelaku('mabcvabc')); // 2
+console.log(cariPelaku('abcdabdc')); // 1
+console.log(cariPelaku('bcabcac')); // 1
+console.log(cariPelaku('abcabcabc')); // 3
+console.log(cariPelaku('babcbacabc')); // 2
